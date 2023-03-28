ea1424f82ee066d4ef5137b54dbd07876f344a08
daadaadaah <daadaadaah@gmail.com>
2023-03-28 21:54:42 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/hcommerce/heecommerce/HeeCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hcommerce.heecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeeCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeeCommerceApplication.class, args);
+	}
+
+}
