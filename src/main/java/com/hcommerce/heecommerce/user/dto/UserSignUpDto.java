86197fe61362ab264733f9ba504e4cfa9ef03e3d
김태희 <godtaeheedev@gmail.com>
2023-04-30 04:09:24 +0900

    Feat: Sign-Up SQL 추가

--- src/main/java/com/hcommerce/heecommerce/user/dto/UserSignUpDto.java
+++ src/main/java/com/hcommerce/heecommerce/user/dto/UserSignUpDto.java
@@ -1,12 +1,29 @@
 package com.hcommerce.heecommerce.user.dto;
 
+import jakarta.annotation.Nullable;
 import lombok.Getter;
+import lombok.NonNull;
 
 @Getter
 public class UserSignUpDto {
 
     private Long id;
 
+    @NonNull
     private String phoneNumber;
 
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String mainAddress;
+
+    @Nullable
+    private String detailAddress;
+
+    public UserSignUpDto(@NonNull String phoneNumber, @NonNull String password, @NonNull String mainAddress) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.mainAddress = mainAddress;
+    }
 }
