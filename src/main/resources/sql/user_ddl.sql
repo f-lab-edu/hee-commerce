8168c0864722aaa4dd38a316b853bebb232c62bc
김태희 <godtaeheedev@gmail.com>
2023-04-30 03:46:19 +0900

    Feat: phoneNumber, password, mainAddress Not NULL 조건 추가

--- src/main/resources/sql/user_ddl.sql
+++ src/main/resources/sql/user_ddl.sql
@@ -2,9 +2,9 @@ create table user
 (
     user_id        bigint auto_increment
         primary key,
-    phone_number   varchar(500)                        null,
-    password       varchar(500)                        null,
-    main_address   varchar(500)                        null,
+    phone_number   varchar(500)                        not null,
+    password       varchar(500)                        not null,
+    main_address   varchar(500)                        not null,
     detail_address varchar(500)                        null,
     created_at     timestamp default CURRENT_TIMESTAMP null,
     deleted_at     timestamp                           null
