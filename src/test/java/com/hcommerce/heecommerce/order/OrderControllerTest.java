bf76d76c81cb5b308bab22774ef458fd2f58227c
daadaadaah <daadaadaah@gmail.com>
2023-04-30 13:18:19 +0900

    OrderController 예외 추가 중

--- src/test/java/com/hcommerce/heecommerce/order/OrderControllerTest.java
+++ src/test/java/com/hcommerce/heecommerce/order/OrderControllerTest.java
@@ -35,4 +35,28 @@ class OrderControllerTest {
         resultActions.andExpect(status().isOk())
                 .andDo(OrderControllerRestDocs.completeOrderReceipt());
     }
+
+    @Test
+    @DisplayName("UUID 형식에 어긋난 order_uuid로 요청한 경우")
+    void completeOrderReceiptWithOrderUuidIsNotUuidType() throws Exception {
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                patch("/admin/orders/{order_uuid}/order-receipt-complete", 123)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("UUID 형식에 맞지만, DB에 없는 order_uuid로 요청한 경우")
+    void completeOrderReceiptWithOrderUuidIsNotInDB() throws Exception {
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                patch("/admin/orders/{order_uuid}/order-receipt-complete", 123)
+        );
+
+        // then
+        resultActions.andExpect(status().isNotFound());
+    }
 }
