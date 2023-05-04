8302d1665b1106bce0ea5e1a03c6fa02bc95ae1b
daadaadaah <daadaadaah@gmail.com>
2023-05-05 01:45:14 +0900

    Fix: SuccessMessageResponseDto -> CommandSuccessResponseDto 수정

--- src/main/java/com/hcommerce/heecommerce/common/CommandSuccessResponseDto.java
+++ src/main/java/com/hcommerce/heecommerce/common/CommandSuccessResponseDto.java
@@ -5,6 +5,6 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class SuccessMessageResponseDto {
+public class CommandSuccessResponseDto {
     private final String message;
 }
