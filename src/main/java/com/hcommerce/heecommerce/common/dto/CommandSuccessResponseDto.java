5c057ee29a670022c8fc343802efbaeab599a6c3
daadaadaah <daadaadaah@gmail.com>
2023-05-05 01:51:32 +0900

    Feat: 공통으로 사용할 CommandSuccessResponseDto 추가

---
+++ src/main/java/com/hcommerce/heecommerce/common/dto/CommandSuccessResponseDto.java
@@ -0,0 +1,10 @@
+package com.hcommerce.heecommerce.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommandSuccessResponseDto {
+    private final String message;
+}
