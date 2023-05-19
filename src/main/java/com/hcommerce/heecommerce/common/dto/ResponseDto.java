8ad0ab18ea8113a2121146e2f5e6261867a986dd
daadaadaah <daadaadaah@gmail.com>
2023-05-19 23:05:10 +0900

    ResponseDto에 data 필드 추가

--- src/main/java/com/hcommerce/heecommerce/common/dto/ResponseDto.java
+++ src/main/java/com/hcommerce/heecommerce/common/dto/ResponseDto.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 public class ResponseDto {
     private final String code;
     private final String message;
+    private final Object data;
 
     @Builder
-    public ResponseDto(String code, String message) {
+    public ResponseDto(String code, String message, Object data) {
         this.code = code;
         this.message = message;
+        this.data = data;
     }
 }
