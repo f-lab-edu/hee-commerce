0fcf69aa34a9901e72cd229a67c6c65811cdfced
daadaadaah <daadaadaah@gmail.com>
2023-06-09 14:58:55 +0900

    `Redis` 추가
    - 의존성 설치 및 설정 추가
    - `RedisSortSetRepository` 추가
    - `RedisHashRepository` 추가

---
+++ src/main/java/com/hcommerce/heecommerce/common/dao/RedisHashRepository.java
@@ -0,0 +1,61 @@
+package com.hcommerce.heecommerce.common.dao;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.concurrent.TimeUnit;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RedisHashRepository<T> {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private final HashOperations<String, String, String> hashOperations;
+
+    public RedisHashRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.hashOperations = redisTemplate.opsForHash();
+    }
+
+    public T findAllByKey(String key, String hashKey, TypeReference<T> objectTypeReference) {
+        String item = (String) hashOperations.get(key, hashKey);
+
+        return convertObjectFromString(item, objectTypeReference);
+    }
+
+    public void save(String key, String hashKey, T item) {
+        String itemString = convertStringFromObject(item);
+
+        hashOperations.put(key, hashKey, itemString);
+    }
+
+    public void saveWithExpirationTime(String key, String hashKey, T item, long time, TimeUnit timeUnit) {
+        String itemString = convertStringFromObject(item);
+
+        hashOperations.put(key, hashKey, itemString);
+
+        redisTemplate.expire(key, time, timeUnit);
+    }
+
+    private T convertObjectFromString(String item, TypeReference<T> objectTypeReference) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.readValue(item, objectTypeReference);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String convertStringFromObject(T item) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.writeValueAsString(item);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/hcommerce/heecommerce/common/dao/RedisSortSetRepository.java
@@ -0,0 +1,34 @@
+package com.hcommerce.heecommerce.common.dao;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RedisSortSetRepository<T> {
+
+    private final RedisTemplate<String, T> redisTemplate;
+
+    private final ZSetOperations<String, T> zSetOps;
+
+    public RedisSortSetRepository(RedisTemplate<String, T> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.zSetOps = redisTemplate.opsForZSet();
+    }
+
+    public Set<T> findAll(String key) {
+        return zSetOps.range(key, 0, -1);
+    }
+
+    public void save(String key, T item, int score) {
+        zSetOps.add(key, item, score);
+    }
+
+    public void saveWithExpirationTime(String key, T item, int score, long time, TimeUnit timeUnit) {
+        zSetOps.add(key, item, score);
+
+        redisTemplate.expire(key, time, timeUnit);
+    }
+}
