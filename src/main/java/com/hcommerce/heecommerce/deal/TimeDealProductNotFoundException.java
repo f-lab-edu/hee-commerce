91e844375843ced6c00505d6408b0aab43fb6c66
daadaadaah <daadaadaah@gmail.com>
2023-06-25 01:11:20 +0900

    `placeOrder`의 유효성 검증 중 `validateHasDealProductUuid` 추가

---
+++ src/main/java/com/hcommerce/heecommerce/deal/TimeDealProductNotFoundException.java
@@ -0,0 +1,12 @@
+package com.hcommerce.heecommerce.deal;
+
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TimeDealProductNotFoundException extends RuntimeException {
+    public TimeDealProductNotFoundException(UUID orderUuid) {
+        super(orderUuid + " : 해당 타임 딜 상품을 찾을 수 없습니다.");
+    }
+}
--- src/main/java/com/hcommerce/heecommerce/order/OrderService.java
+++ src/main/java/com/hcommerce/heecommerce/order/OrderService.java
@@ -1,18 +1,25 @@
 package com.hcommerce.heecommerce.order;
 
+import com.hcommerce.heecommerce.deal.DealQueryRepository;
+import com.hcommerce.heecommerce.deal.TimeDealProductNotFoundException;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class OrderService {
 
     private OrderCommandRepository orderRepository;
 
+    private DealQueryRepository dealQueryRepository;
+
     @Autowired
-    public OrderService(OrderCommandRepository orderRepository) {
+    public OrderService(
+        OrderCommandRepository orderRepository,
+        DealQueryRepository dealQueryRepository
+    ) {
         this.orderRepository = orderRepository;
+        this.dealQueryRepository = dealQueryRepository;
     }
 
     public void completeOrderReceipt(UUID orderUuid) {
@@ -21,5 +28,35 @@ public class OrderService {
         if(resultOrderUuid == null) {
             throw new OrderNotFoundException(orderUuid);
         }
+    }
+
+    public void placeOrder(OrderForm orderForm) {
+        validateOrderForm(orderForm);
+    }
+
+    private void validateOrderForm(OrderForm orderForm) {
+        validateHasDealProductUuid(orderForm.getDealProductUuid());
+
+        // TODO : Mybatis 연동이 필요하므로, 다른 PR에서 작업할 예정
+        validateHasUserId(orderForm.getUserId());
+
+        // TODO : RedisRepository에 추가적인 함수 필요하므로, 다른 PR에서 작업할 예정
+        validateOrderQuantityInMaxOrderQuantityPerOrder(orderForm.getOrderQuantity());
+    }
+
+    private void validateHasDealProductUuid(UUID dealProductUuid) {
+        boolean hasDealProductUuid = dealQueryRepository.hasDealProductUuid(dealProductUuid);
+
+        if(!hasDealProductUuid) {
+            throw new TimeDealProductNotFoundException(dealProductUuid);
+        }
+    }
+
+    private void validateHasUserId(int userId) {
+
+    }
+
+    private void validateOrderQuantityInMaxOrderQuantityPerOrder(int orderQuantity) {
+
     }
 }
