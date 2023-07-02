fbc9812e8175742fcb3479ebc9c95d45fc9374db
daadaadaah <daadaadaah@gmail.com>
2023-07-03 03:06:02 +0900

    `SessionCallback`을 이용해서 `ERR DISCARD without MULTI 이슈` 해결

--- src/main/java/com/hcommerce/heecommerce/deal/DealCommandRepository.java
+++ src/main/java/com/hcommerce/heecommerce/deal/DealCommandRepository.java
@@ -9,11 +9,15 @@ import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -89,55 +93,66 @@ public class DealCommandRepository {
 
     /**
      * saveTimeDealProduct 는 타임딜 상품을 저장하는 함수이다.
+     *
+     * SessionCallback 을 사용한 이유는 https://github.com/f-lab-edu/hee-commerce/issues/91 참고
      */
     private void saveTimeDealProduct(String dealOpenDate, int scoreForSortSet, Instant startedAt, Instant finishedAt) {
-        try {
+        redisTemplate.execute(new SessionCallback<List<Object>>() {
+            @Override
+            public List<Object> execute(RedisOperations operations) throws DataAccessException {
+                try {
 
-            // --- 트랜잭션 시작
-            redisTemplate.multi();
+                    // --- 트랜잭션 시작
+                    operations.multi();
 
-            int PAGE_NUMBER = 0;
+                    int PAGE_NUMBER = 0;
 
-            String dealProductUuid = UUID.randomUUID().toString();
+                    String dealProductUuid = UUID.randomUUID().toString();
 
-            int expirationInSeconds = 86_400; // TODO : 일단 임시로 24시간으로 설정, 유효시간을 설정해서 관리할 것인가? 아니면 따로 lambda나 batch 서버를 두어서 삭제시킬 것인가?
+                    int expirationInSeconds = 86_400; // TODO : 일단 임시로 24시간으로 설정, 유효시간을 설정해서 관리할 것인가? 아니면 따로 lambda나 batch 서버를 두어서 삭제시킬 것인가?
 
-            saveDealProductUuids(
-                DealType.TIME_DEAL+":"+dealOpenDate+":"+PAGE_NUMBER,
-                dealProductUuid,
-                scoreForSortSet,
-                expirationInSeconds
-            );
+                    saveDealProductUuids(
+                        DealType.TIME_DEAL+":"+dealOpenDate+":"+PAGE_NUMBER,
+                        dealProductUuid,
+                        scoreForSortSet,
+                        expirationInSeconds
+                    );
 
-            saveDealProductEntity(
-                "timeDealProducts:"+dealOpenDate,
-                dealProductUuid,
-                TimeDealProductEntity.builder()
-                    .dealProductUuid(UUID.fromString(dealProductUuid))
-                    .dealProductTile("1000원 할인 상품 "+scoreForSortSet)
-                    .productMainImgThumbnailUrl("/main_thumbnail_test.png")
-                    .productOriginPrice(1000*(scoreForSortSet+1))
-                    .dealProductDiscountType(DiscountType.FIXED_AMOUNT)
-                    .dealProductDiscountValue(1000)
-                    .productDetailImgUrls(new String[]{"/detail_test1.png", "/detail_test2.png", "/detail_test3.png", "/detail_test4.png", "/detail_test5.png"})
-                    .productMainImgUrl("/main_test.png")
-                    .maxOrderQuantityPerOrder(10)
-                    .startedAt(startedAt)
-                    .finishedAt(finishedAt)
-                    .build(),
-                expirationInSeconds
-            );
+                    if(scoreForSortSet == 1) {
+                        throw new RuntimeException("예외다");
+                    }
 
-            saveInventory(
-                "timeDealProductInventory:"+dealProductUuid,
-                10 // TODO : 일단 통일 시킴
-            );
+                    saveDealProductEntity(
+                        "timeDealProducts:"+dealOpenDate,
+                        dealProductUuid,
+                        TimeDealProductEntity.builder()
+                            .dealProductUuid(UUID.fromString(dealProductUuid))
+                            .dealProductTile("1000원 할인 상품 "+scoreForSortSet)
+                            .productMainImgThumbnailUrl("/main_thumbnail_test.png")
+                            .productOriginPrice(1000*(scoreForSortSet+1))
+                            .dealProductDiscountType(DiscountType.FIXED_AMOUNT)
+                            .dealProductDiscountValue(1000)
+                            .productDetailImgUrls(new String[]{"/detail_test1.png", "/detail_test2.png", "/detail_test3.png", "/detail_test4.png", "/detail_test5.png"})
+                            .productMainImgUrl("/main_test.png")
+                            .maxOrderQuantityPerOrder(10)
+                            .startedAt(startedAt)
+                            .finishedAt(finishedAt)
+                            .build(),
+                        expirationInSeconds
+                    );
 
-            redisTemplate.exec();
-        } catch (Exception e) {
-            redisTemplate.discard();
-            throw e;
-        }
+                    saveInventory(
+                        "timeDealProductInventory:"+dealProductUuid,
+                        10 // TODO : 일단 통일 시킴
+                    );
+
+                    return operations.exec();
+                } catch (Exception e) {
+                    operations.discard();
+                    throw e;
+                }
+            }
+        });
     }
 
     private void saveDealProductUuids(String key, String value, int score, long expirationInSeconds) {
