a4d7cceec4f2b234c1774ed2eec2862b8d3006c7
daadaadaah <daadaadaah@gmail.com>
2023-07-10 03:04:59 +0900

    좀더 의미가 명확한 네이밍으로 OrderStatus Enum 값 수정
    - 불필요한 Enum 값 삭제

--- src/main/java/com/hcommerce/heecommerce/order/OrderStatus.java
+++ src/main/java/com/hcommerce/heecommerce/order/OrderStatus.java
@@ -5,10 +5,9 @@ package com.hcommerce.heecommerce.order;
  * TODO : 일단 생각나는 상태들만 추가하였고, 추후에 확장성을 고려해서 더 다양한 상태를 담을 수 있고, 상태 간의 전환에 대한 예외처리도 생각해볼 예정
  */
 public enum OrderStatus {
-    ORDER_RECEIPT_COMPLETE, // TODO : 삭제 예정
     PAYMENT_PENDING,
-    PAYMENT_COMPLETED,
-    PAYMENT_CANCELLED,
+    ORDER_COMPLETED,
+    ORDER_CANCELLED,
     DELIVERY_PENDING,
     IN_DELIVERY,
     DELIVERY_COMPLETED
