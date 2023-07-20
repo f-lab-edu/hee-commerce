1302f1b66e42d954ef8ffb3ae63fb3bdbc835db5
daadaadaah <daadaadaah@gmail.com>
2023-07-20 19:09:10 +0900

    time_deal_product_inventory_event_history 에 오타 수정

--- src/main/resources/db/migration/V1.4__add_deal_product_inventory_event_history.sql
+++ src/main/resources/db/migration/V1.4__add_deal_product_inventory_event_history.sql
@@ -1,11 +1,11 @@
-drop table if exists `deal_product_inventory_event_history`;
+drop table if exists `time_deal_product_inventory_event_history`;
 
 -- time_deal_product_inventory_event_history 는 딜 상품 재고 이벤트 히스토리를 저장하는 테이블로, 데이터 업데이트 없이 데이터를 추가만 하는 테이블이다.
 -- time_deal_product 테이블에 inventory 필드가 있는데, 이 테이플이 필요한 이유는 https://docs.google.com/document/d/1JlKVZVZQwWjxF3bbVW5a4K3YmnfC6gYYoMHhAn5JKLs/edit 참고
 CREATE TABLE `time_deal_product_inventory_event_history` (
   `inventory_event_history_id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT, # 재고 이벤트 히스토리 식별자
   `deal_product_uuid` binary(16) NOT NULL, # 딜 상품 식별자
-  `order_uuid` binary(16) NOT NULL, # 딜 상품 식별자
+  `order_uuid` binary(16) NOT NULL, # 주문 식별자
   `inventory` int NOT NULL, # 이벤트로 처리된 재고 수량
   `previous_deal_quantity` int NOT NULL, # 이벤트 전 재고 수량
   `inventory_event_type` varchar(255) NOT NULL, # 이벤트 유형(예 : 주문 접수, 주문 취소)
