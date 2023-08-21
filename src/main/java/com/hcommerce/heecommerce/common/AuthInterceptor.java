d2f9614587bd83b7497adfe056495e29217cd6d6
daadaadaah <daadaadaah@gmail.com>
2023-08-22 00:20:48 +0900

    Jwt 추가에 따른 `AuthInterceptor` 수정

--- src/main/java/com/hcommerce/heecommerce/common/AuthInterceptor.java
+++ src/main/java/com/hcommerce/heecommerce/common/AuthInterceptor.java
@@ -1,57 +1,44 @@
 package com.hcommerce.heecommerce.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hcommerce.heecommerce.auth.AuthenticationService;
 import com.hcommerce.heecommerce.common.dto.ResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 public class AuthInterceptor implements HandlerInterceptor {
+
+    private final AuthenticationService authenticationService;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Autowired
+    public AuthInterceptor(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        // TODO : 추후 삭제 필요! 이거 주석처리 후 테스트해야 제대로 테스트 됨
-//        HttpSession session = request.getSession();
-//        session.setAttribute("isAdmin", true);
-
-        if (!AuthHelper.isLogin(request)) {
+        if(!authenticationService.isAuthenticatedUser(request)) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
             response.setContentType("application/json; charset=UTF-8");
 
             response.getWriter().append(
-                    objectMapper.writeValueAsString(ResponseDto.builder()
-                            .code(HttpStatus.UNAUTHORIZED.name())
-                            .message("로그인 후에 이용할 수 있습니다.")
-                            .build())
+                objectMapper.writeValueAsString(ResponseDto.builder()
+                    .code(HttpStatus.UNAUTHORIZED.name())
+                    .message("로그인 후에 이용할 수 있습니다.")
+                    .build())
             );
 
-            return false;
-        }
-
-        if(!AuthHelper.isAdmin(request)) {
-            response.setStatus(HttpStatus.FORBIDDEN.value());
-
-            response.setContentType("application/json; charset=UTF-8");
-
-            response.getWriter().append(
-                    objectMapper.writeValueAsString(ResponseDto.builder()
-                            .code(HttpStatus.FORBIDDEN.name())
-                            .message("관리자만 이용 가능합니다.")
-                            .build())
-            );
             return false;
         }
 
         return true;
-    }
-
-    // TODO : 추후 삭제 필요!
-    private boolean isRandomAdmin() {
-        return System.currentTimeMillis() % 2 == 0;
     }
 }
--- src/test/java/com/hcommerce/heecommerce/common/AuthInterceptorTest.java
+++ src/test/java/com/hcommerce/heecommerce/common/AuthInterceptorTest.java
@@ -1,69 +1,64 @@
 package com.hcommerce.heecommerce.common;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+
+import com.hcommerce.heecommerce.auth.AuthenticationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockHttpSession;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @DisplayName("AuthInterceptor")
-class AuthInterceptorTest {
+public class AuthInterceptorTest {
 
+    @InjectMocks
     private AuthInterceptor authInterceptor;
+
+    @Mock
+    private AuthenticationService authenticationService;
+
     private MockHttpServletRequest request;
+
     private MockHttpServletResponse response;
-    private MockHttpSession session;
+
+    private MockMvc mockMvc;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         MockitoAnnotations.openMocks(this);
-        authInterceptor = new AuthInterceptor();
+        mockMvc = MockMvcBuilders.standaloneSetup(authInterceptor).build();
+
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
-        session = new MockHttpSession();
     }
 
     @Nested
-    @DisplayName("when login user is admin")
-    class Context_LoginUser_is_Admin {
+    @DisplayName("when user is login")
+    class Context_user_is_login {
         @Test
         @DisplayName("returns 200 OK")
-        void it_returns_HTTP_200_OK() throws Exception {
+        void It_returns_HTTP_200_OK() throws Exception {
             // given
-            session.setAttribute("isAdmin", true);
-            request.setSession(session);
+            given(authenticationService.isAuthenticatedUser(request)).willReturn(true);
 
             // when
             boolean result = authInterceptor.preHandle(request, response, null);
 
             // then
             assertTrue(result);
+
             assertEquals(HttpStatus.OK.value(), response.getStatus());
-        }
-    }
-
-    @Nested
-    @DisplayName("when login user is not admin")
-    class Context_LoginUser_is_Not_Admin {
-        @Test
-        @DisplayName("returns 403 error")
-        void it_returns_403_Error() throws Exception {
-            // given
-            session.setAttribute("isAdmin", false);
-            request.setSession(session);
-
-            // when
-            boolean result = authInterceptor.preHandle(request, response, null);
-
-            // then
-            assertFalse(result);
-            assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
         }
     }
 
@@ -72,15 +67,16 @@ class AuthInterceptorTest {
     class Context_User_is_Not_Login {
         @Test
         @DisplayName("returns 401 error")
-        void it_returns_401_Error() throws Exception {
+        void It_returns_401_Error() throws Exception {
             // given
-            request.setSession(null);
+            given(authenticationService.isAuthenticatedUser(request)).willReturn(false);
 
             // when
             boolean result = authInterceptor.preHandle(request, response, null);
 
             // then
             assertFalse(result);
+
             assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
         }
     }
